c4353fefc47e42c4e5b24d330b8b25fa683a2923
ThomasJockin <thomas@thomasjockin.com>
2025-08-26 07:07:18 -0400

    Exhibition Links
    Adding subpages for NFC tag function. Method is messy but it works.

---
+++ app/exhibit11/page.jsx
@@ -0,0 +1,40 @@
+"use client"
+
+import {useState} from "react";
+ 
+export default function Page() {
+     const [userAnswer, storeAnswer] = useState(null);
+    const check = userAnswer === prompt.user_type;
+    
+    let response = "img-item"; // Default class
+    if (userAnswer === null) {
+        response = "img-item";
+    } else if (check) {
+        response = "img-item-yes";
+    } else {
+        response = "img-item-no";
+    }
+
+         function Answer({ choice, actualAnswer }) {
+    if (choice === null) {
+        return null;
+    }
+
+    if (choice === actualAnswer) {
+        return <div><label className="exhibitanswer">Correct. This is <strong>{actualAnswer} made by Miguel Escamilla</strong>.</label></div>;
+    }
+
+    return <div><label className="exhibitanswer">Nope. This is <strong>{actualAnswer} made by Miguel Escamilla</strong>. </label></div>;
+}
+  return (
+    <div className="exhibitshow">
+            <div className="response-buttons">
+            <button onClick={() => storeAnswer("human")}disabled={userAnswer !== null}>Human</button>
+            <button id="ai"  onClick={() => storeAnswer("ai")}disabled={userAnswer !== null}>Ai</button>
+            </div>
+            <Answer choice={userAnswer} actualAnswer={"human"} />
+    </div>
+      ) 
+
+ 
+}
